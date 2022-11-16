e25cdb56f08292a7483e9e84989a84f92235412c
Kevin Matthes <92332892+kevinmatthes@users.noreply.github.com>
2022-11-16 19:30:38 +0100

    Update tautocomplete.nim

--- tests/tautocomplete.nim
+++ tests/tautocomplete.nim
@@ -1,5 +1,5 @@
-import std/[unittest, macros]
-include moepkg/autocomplete
+import std/[macros, options, sequtils, sugar, unittest]
+import moepkg/[autocomplete, unicodeext]
 
 const code = ru"""proc fibonacci(n: int): int =
   if n == 0: return 0
